7ed3a9c0b71c3b40cc43c448d61a7b761aae4fed
wangming1993 <wangming19932008@163.com>
2018-04-03 19:32:19 +0800

    feat: use ejs to render html

--- example.js
+++ example.js
@@ -1,6 +1,8 @@
 const puppeteer = require('puppeteer');
+const ejs = require("ejs");
 
 (async () => {
+
   const browser = await puppeteer.launch({
     args: [
       '--start-maximized',
@@ -9,42 +11,37 @@ const puppeteer = require('puppeteer');
   });
   const page = await browser.newPage();
 
-  let width = 400
+  let width = 375;
   await page.setViewport({
     width: width,
     height: 0,
     isMobile: true,
   });
 
-  const html =  `
-  <!doctype html>
-  <html lang="en">
-    <head>
-      <meta charset="utf-8">
-    </head>
-    <body>
-      <img src="https://images2018.cnblogs.com/news/66372/201804/66372-20180402201814545-853809727.jpg" />
-    </body>
-  </html>
-`;
+  let str=require("fs").readFileSync(__dirname+"/share.ejs","utf8")  //先读文件
+  let content =ejs.render(str,{
+      names:["cd","lw"],       //第一个参数是给ejs渲染的内容
+      avatar: "https://avatars2.githubusercontent.com/u/5611286?s=400&u=f95f36b2db0990774b22d87f7eefcba2cb14082e&v=4",
+      name: "痕无落",
+      desc: "谁人与你立黄昏，谁人问你粥可温",
+      qrcode: "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1522759741315&di=22c673f7a0870c2eb00380c554d9bdaa&imgtype=0&src=http%3A%2F%2Fimg.mp.itc.cn%2Fupload%2F20170503%2F3968d861dbfb453e9ac470573d50a43d_th.jpeg",
+      feedType: "link",  // text / image / link
+      feedText: "哈哈哈哈哈哈哈哈哈哈哈哈  好长啊 哈哈哈哈哈哈",
+      feedImages: [
+        "https://avatars2.githubusercontent.com/u/5611286?s=400&u=f95f36b2db0990774b22d87f7eefcba2cb14082e&v=4",
+        "https://avatars2.githubusercontent.com/u/5611286?s=400&u=f95f36b2db0990774b22d87f7eefcba2cb14082e&v=4",
+        "https://avatars2.githubusercontent.com/u/5611286?s=400&u=f95f36b2db0990774b22d87f7eefcba2cb14082e&v=4",
+        "https://avatars2.githubusercontent.com/u/5611286?s=400&u=f95f36b2db0990774b22d87f7eefcba2cb14082e&v=4",
+      ],
+      feedTitle: "初次见面,余生请指教!!!"
+  })
 
-  await page.goto('file:///Users/wangming/workspace/byzai/headless/share.html', {
+  await page.goto(`data:text/html,${content}`, {
     waitUntil: [
-      'networkidle2',
+      'domcontentloaded',
+      'load',
     ],
-  });
-  // await page.goto(`data:text/html,${html}`, {
-  //   waitUntil: [
-  //     'domcontentloaded',
-  //     'load',
-  //   ],
-  // })
-
-  // let result = await page.evaluate(
-  //   () => {
-  //     return window.innerWidth;
-  //   }
-  // );
+  })
 
   let result = await page.evaluate(
     () => {
@@ -55,6 +52,7 @@ const puppeteer = require('puppeteer');
   await page.screenshot({path: 'example.png', fullPage: false,
     clip:{x:0, y:0, width:width, height: result}
   });
+
   await browser.close();
 
   console.log(`Detected window.innerWidth to be ${result}.`);
